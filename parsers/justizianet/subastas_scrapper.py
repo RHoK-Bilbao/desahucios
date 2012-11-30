955f0f9c8caf8b379074a8003955de297de6f98b
Unai Aguilera <gkalgan@gmail.com>
2012-11-30 18:16:08 +0100

    Matching string with lower(). Fixing @jaimeirurzun's code.

--- parsers/justizianet/subastas_scrapper.py
+++ parsers/justizianet/subastas_scrapper.py
@@ -191,7 +191,7 @@ def scrappEviction(cpartido, location, url, title, cancelled):
     evicdict['Cancelado'] = cancelled
 
     for municipio in municipios:
-        if municipio in title:
+        if municipio.lower() in title.lower():
             evicdict['Municipio'] = municipio
             break
 
@@ -218,3 +218,4 @@ import operator
 sorted_wordlist = sorted(wordcounter.iteritems(), key=operator.itemgetter(1))
 for word in sorted_wordlist:
     print word'''
+
