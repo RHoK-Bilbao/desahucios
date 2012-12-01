5809856dbff085b0f4b262289cbf1c1592e9f757
Pablo Orduña <pablo.orduna@deusto.es>
2012-12-01 10:35:19 +0100

    Add instalacion.py

---
+++ website/instalacion.py
@@ -0,0 +1,39 @@
+import urllib2
+import os
+import getpass
+import traceback
+
+os.system("pip install -r requirements.txt")
+
+rhok_db = urllib2.urlopen("http://dev.morelab.deusto.es/rhok_desahucios.sql").read()
+
+open("rhok_desahucios.sql",'w').write(rhok_db)
+
+user = "root"
+password = getpass.getpass("Dame password mysql")
+
+import pymysql as dbi
+print "Creating database..."
+try:
+    connection = dbi.connect(user=user, passwd=passwd, host="127.0.0.1")
+    cursor = connection.cursor()
+    cursor.execute("""CREATE DATABASE IF NOT EXISTS `rhok_desahucios` ;
+    CREATE USER 'rhok'@'localhost' IDENTIFIED BY 'rhok';
+    GRANT ALL PRIVILEGES ON `rhok_desahucios`.* TO `rhok`@`localhost`;""")
+    connection.commit()
+    cursor.close()
+    connection.close()
+except:
+    traceback.print_exc()
+
+print "done"
+print "Adding content..."
+try:
+    connection = dbi.connect(user=user, passwd=passwd, host="127.0.0.1")
+    cursor = connection.cursor()
+    cursor.execute(rhok_db)
+    connection.commit()
+    cursor.close()
+    connection.close()
+except:
+    traceback.print_exc()
